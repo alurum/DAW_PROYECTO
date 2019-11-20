3a4e3c071737472ce9d841d6573ad7a704b0b38c
Lalo <Lalo@DESKTOP-NJGC4AH>
2019-11-20 17:47:20 -0600

    Modifying in order to eliminate unnecessary files.

--- src/java/controller/CtlLogin.java
+++ src/java/controller/CtlLogin.java
@@ -20,7 +20,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.dao.DaoUsuario;
 import model.entities.Asociado;
 import model.entities.Rol;
 import model.sessions.AsociadoFacade;
